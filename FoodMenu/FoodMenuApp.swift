ac4fcc9dcbdbd3b2ec4b9ca972de0ba739a3dfe4
Mekonnen <mtesfazien@sfsu.edu>
2024-03-23 20:02:41 -0700

    Initial Commit

---
+++ FoodMenu/FoodMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMenuApp.swift
+//  FoodMenu
+//
+//  Created by patron on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
