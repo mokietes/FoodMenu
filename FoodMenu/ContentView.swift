e40de83f12e9832c60e0fff052b8b19eb0e73e40
Mekonnen <mtesfazien@sfsu.edu>
2024-03-26 21:19:03 -0700

    Tab view

--- FoodMenu/ContentView.swift
+++ FoodMenu/ContentView.swift
@@ -6,26 +6,25 @@
 //
 
 import SwiftUI
-enum Tab {
-    case profile
-}
+
 
 struct ContentView: View {
+    
+    @State private var selectTab: Tab = .profile
+    
+    enum Tab {
+        case profile
+        case search
+        case scan
+    }
+    
     var body: some View {
-        VStack {
-            ProfileView()
-                .tabItem{
-                    
-                    Image(systemName: "person")
-                    Text("Profile")
-                }
-            
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        TabView(selection: $selectTab,
+                content:  {
+            Text("Tab Content 1").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 1")/*@END_MENU_TOKEN@*/ }.tag(1)
+            Text("Tab Content 2").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 2")/*@END_MENU_TOKEN@*/ }.tag(2)
+            Text("Tab Content 2").tabItem { Text("Tab Label 3") }.tag(3)
+        })
     }
 }
 
