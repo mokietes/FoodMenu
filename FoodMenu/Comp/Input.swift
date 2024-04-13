e65a1e174d77225f1f81effd08b9d125acd31e26
Mekonnen <mtesfazien@sfsu.edu>
2024-04-12 20:36:23 -0700

    Login

--- FoodMenu/Comp/Input.swift
+++ FoodMenu/Comp/Input.swift
@@ -7,12 +7,33 @@
 
 import SwiftUI
 
+
 struct Input: View {
+    @Binding var text: String
+    let title: String
+    let placeholder: String
+    var isSecureField = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 12){
+            Text(title)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .font(.footnote)
+            if isSecureField {
+                SecureField(placeholder, text: $text)
+                    .font(.system(size: 14))
+                
+            }else {
+                TextField(placeholder, text: $text)
+                    .font(.system(size: 14))
+                
+            }
+            Divider()
+        }
     }
 }
 
 #Preview {
-    Input()
+    Input(text: .constant(""), title: "Email", placeholder: "name@example.com")
 }
