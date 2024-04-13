bdf86b5427056281b55fea2b094226bb9bb28558
Mekonnen <mekonnen.tesfamichael@gmail.com>
2024-04-12 21:13:47 -0700

    signup button

---
+++ FoodMenu/Comp/SignupButton.swift
@@ -0,0 +1,18 @@
+//
+//  SignupButton.swift
+//  FoodMenu
+//
+//  Created by patron on 4/12/24.
+//
+
+import SwiftUI
+
+struct SignupButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignupButton()
+}
