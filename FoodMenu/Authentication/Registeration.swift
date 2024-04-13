df3d7f728724dfbbcb085da7de40b40b215456e7
Mekonnen <mekonnen.tesfamichael@gmail.com>
2024-04-12 20:57:54 -0700

    registeration

---
+++ FoodMenu/Authentication/Registeration.swift
@@ -0,0 +1,18 @@
+//
+//  Registeration.swift
+//  FoodMenu
+//
+//  Created by patron on 4/12/24.
+//
+
+import SwiftUI
+
+struct Registeration: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Registeration()
+}
