aa670b50855b7b092912e4ecff462d57f7f156c0
Mekonnen <mekonnen.tesfamichael@gmail.com>
2024-04-16 23:28:34 -0700

    settingButton

---
+++ FoodMenu/Comp/SettingButton.swift
@@ -0,0 +1,18 @@
+//
+//  SettingButton.swift
+//  FoodMenu
+//
+//  Created by patron on 4/16/24.
+//
+
+import SwiftUI
+
+struct SettingButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingButton()
+}
