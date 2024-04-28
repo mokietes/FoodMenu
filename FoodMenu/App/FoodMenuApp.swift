81a0316ce6b9a7cbd05ca32e08fdd28a1635197b
Mekonnen <mekonnen.tesfamichael@gmail.com>
2024-04-27 20:12:28 -0700

    pkg

--- FoodMenu/App/FoodMenuApp.swift
+++ FoodMenu/App/FoodMenuApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct FoodMenuApp: App {
+    @StateObject var viewModel = AuthModel()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(viewModel)
         }
     }
 }
---
+++ FoodMenu/Authentication/AuthModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthModel.swift
+//  FoodMenu
+//
+//  Created by patron on 4/27/24.
+//
+
+import Foundation
