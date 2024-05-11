f9aadde48ee412d329649a8d943e9bb7c0e40de0
Mekonnen <mekonnen.tesfamichael@gmail.com>
2024-05-10 23:50:29 -0700

    FoodScanner

---
+++ FoodMenu/View/FoodScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  FoodScannerView.swift
+//  FoodMenu
+//
+//  Created by patron on 5/10/24.
+//
+
+import SwiftUI
+
+struct FoodScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FoodScannerView()
+}
