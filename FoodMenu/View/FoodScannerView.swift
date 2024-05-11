0bc90ed708be7609442fd116c3b24f0e2c81226d
Mekonnen <mekonnen.tesfamichael@gmail.com>
2024-05-10 23:57:04 -0700

    bug fix

--- FoodMenu/View/FoodScannerView.swift
+++ FoodMenu/View/FoodScannerView.swift
@@ -8,10 +8,45 @@
 import SwiftUI
 
 struct FoodScannerView: View {
+    
+    @State private var scannedCode : String?
+    @State private var food: Food?
+    @State private var isLoading = false
+    @State private var isPopoverVisible = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if isLoading {
+                ProgressView( "Loading...")
+                
+            } else {
+                if let food = food {
+                    Text( "Product Name: \(food.product_name)")
+                    if let ingredients = food.product.ingredients_text {
+                        Text("Prduct Name = \(food.product.product_name)")
+                    }else {
+                            Text ( "Ingredients not available ")
+                        }
+                        Button("Details") {
+                            isPopoverVisible.toggle()
+                        }
+                        .popover(isPresented: $isPopoverVisible, content: {
+                            VStack {
+                                Text ("Ingredients: \(ingredients)")
+                            } else {
+                                Text ("Ingredients not available")
+                            }
+                            Button ("Close") {
+                                isPopoverVisible.toggle()
+                            }
+                        })
+                    }
+                        
+                }
+            }
+        }
     }
-}
+
 
 #Preview {
     FoodScannerView()
