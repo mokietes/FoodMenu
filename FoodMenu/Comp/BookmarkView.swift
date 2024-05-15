7b7739be0e57b4e4e17b84d12118b7211fb19cbd
Mekonnen <mekonnen.tesfamichael@gmail.com>
2024-05-14 18:50:27 -0700

    Final structure

---
+++ FoodMenu/Comp/BookmarkView.swift
@@ -0,0 +1,18 @@
+//
+//  BookmarkView.swift
+//  FoodMenu
+//
+//  Created by patron on 5/14/24.
+//
+
+import SwiftUI
+
+struct BookmarkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookmarkView()
+}
